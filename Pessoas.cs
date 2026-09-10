7e67ab3c67b8217ef200dc857bcf5b13e60f7916
rafaelablasque <rafaela.blasque@edu.senai.br>
2026-09-10 15:00:07 -0300

    exer1

---
+++ Pessoas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    internal class Pessoas
+    {
+        private string _nome;
+
+        public string Nome { 
+            get { return _nome; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(vaule))
+                {
+                    Console.WriteLine("Não Reconhecido");
+                }
+                else
+                {
+                    _nome = value;
+                }
+            }
+        }
+    }
+}
